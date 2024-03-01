0c02610d81388fca31537789666cf9dc42cdd16f
Aditya Nidhonkar <adityanidhonkar2004@gmail.com>
2024-03-01 21:50:52 +0530

    updated

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { IMG_CDN_URL } from "../utils/constants";
 const MovieCard = ({ posterPath }) => {
+  if (!posterPath) return null;
   return (
     <div className=" w-36 md:w-48 p-4 hover:scale-105 hover:duration-100">
       <img alt="Poster" src={IMG_CDN_URL + posterPath}></img>
